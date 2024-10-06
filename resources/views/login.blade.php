3619d360b06e427137f4cff64f252a5d0c9786bb
Divya Shinde <divya.shinde.developer@gmail.com>
2024-10-06 23:03:19 +0530

    something

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Login Page</h2>
+    <div class="row">
+        <div class="col-md-12">
+            <label>Username:</label>
+            <input type="text" name="username">
+        </div>
+        <div class="col-md-12">
+            <label>Password:</label>
+            <input type="text" name="password">
+        </div>
+    </div>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Home Route
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/login',function()
+{
+    return view('login');
 });
